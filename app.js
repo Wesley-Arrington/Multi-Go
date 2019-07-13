dd0b73621f34032add2239a3029ba9a1a131aaea
Wesley Arrington <cdsigma271@gmail.com>
2019-07-13 16:22:19 -0700

    bug fixes

--- app.js
+++ app.js
@@ -3,15 +3,19 @@ const mongoose = require('mongoose');
 const express = require("express");
 const app = express();
 const db = require('./config/keys').mongoURI;
-const test = require("./routes/api/test");
+const bodyParser = require('body-parser');
+const splash = require("./routes/api/splash");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 mongoose
     .connect(db, { useNewUrlParser: true })
     .then(() => console.log("Connected to MongoDB successfully"))
     .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello World"));
-
+// app.get("/", (req, res) => res.send("Hello World"));
+app.use("/", splash);
 
 const port = process.env.PORT || 5000;
 
