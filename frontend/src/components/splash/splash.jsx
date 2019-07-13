2f56b95fc69ed26cb0ff954fc35b88d0ea6c1cb6
Wesley Arrington <cdsigma271@gmail.com>
2019-07-13 15:29:17 -0700

    cleaned up file structure

---
+++ frontend/src/components/splash/splash.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Splash extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Splash Component</h1>
+            </div>
+        )
+    }
+}
