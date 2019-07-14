c1671738e0a3baf041200e2ec3cc1ae0a66ac33e
Wesley Arrington <cdsigma271@gmail.com>
2019-07-14 16:26:37 -0700

    finished skeleton for lobby page

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 
 import Splash from './splash/splash';
 import Game from './game/game'
+import Lobby from './lobby/lobby'
 import './default_styling/reset.css'
 import './default_styling/style.css'
 
@@ -10,6 +11,7 @@ const App = () => (
     <div>
         <Switch>
             <Route exact path="/" component={Splash} />
+            <Route exact path="/lobby" component={Lobby} />
             <Route exact path="/game" component={Game} />
         </Switch>
     </div>
---
+++ frontend/src/components/lobby/edit_game/edit_game.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class EditGame extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Edit Game Component</h1>
+            </div>
+        )
+    }
+}
