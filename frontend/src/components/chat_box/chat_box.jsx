fcce0750454fb86cdd4d5a3acca0197ffe7adce3
Wesley Arrington <cdsigma271@gmail.com>
2019-07-16 11:55:31 -0700

    working websockets

--- frontend/src/components/chat_box/chat_box.jsx
+++ frontend/src/components/chat_box/chat_box.jsx
@@ -2,8 +2,16 @@ import React, { Component } from 'react';
 import ChatBoxNavBar from './chat_box_nav_bar'
 import ChatInput from './chat_input'
 import './chat_box.css';
+import io from 'socket.io-client'
 
 export default class ChatBox extends Component {
+
+    componentDidMount() {
+        const socket = io('http://localhost:5000');
+        socket.emit("join", {name: Math.random()*100});
+        socket.on("someoneJoined", (data) => { console.log(`${data.name} has joined room`) })
+    }
+
     render() {
         return (
             <div className="chat-box">
