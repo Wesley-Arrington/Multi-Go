b164723c64aef01fc1fe5813af61e796a0e2ce17
Wesley Arrington <cdsigma271@gmail.com>
2019-07-16 15:04:34 -0700

    finalized lobby row styling

--- frontend/src/components/splash/lobby_rows/lobby_row.jsx
+++ frontend/src/components/splash/lobby_rows/lobby_row.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class LobbyRow extends Component {
     render() {
         return (
-            <div className="simple-row-center-y-flex-start-padding-20">
+            <div className="lobby-row">
                 <h3 className="lobby-row-title">Server Title</h3>
 
                 <div className="simple-column">
@@ -13,7 +13,7 @@ export default class LobbyRow extends Component {
                 </div>
 
                 <div className="lobby-row-right-items">
-                    <h5>Align Front</h5>
+                    <h5>2/3</h5>
                     <button className="blue-button" id="splash-page-join-lobby-button">Join Lobby</button>
                 </div>
             </div>
