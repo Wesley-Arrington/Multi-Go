d95b2fcf6328d6be34ae8a84b07920f3a0663095
Wesley Arrington <cdsigma271@gmail.com>
2019-08-07 15:39:28 -0700

    correctly saves user between refreshes

--- routes/api/users.js
+++ routes/api/users.js
@@ -71,8 +71,8 @@ router.post('/login', (req, res) => {
             bcrypt.compare(password, user.password)
                 .then(isMatch => {
                     if (isMatch) {
-                        const payload = { id: user.id, name: user.name };
-
+                        const payload = { id: user.id, email: user.email };
+                        // debugger;
                         jwt.sign(
                             payload,
                             keys.secretOrKey,
