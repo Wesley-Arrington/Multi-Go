78a857d8ad8f4a90d15466375c132fa70bf311fc
Kevin Chou <kchou650@gmail.com>
2019-08-12 17:27:32 -0700

    UI move coordination

--- frontend/src/components/nav_bar/logged_in_options.jsx
+++ frontend/src/components/nav_bar/logged_in_options.jsx
@@ -34,7 +34,7 @@ export default class LoggedInOptions extends Component {
     }
 
     rejoinGame() {
-        debugger
+        // debugger
         this.props.history.push(`/game/${this.props.game.id}`);
     }
 
